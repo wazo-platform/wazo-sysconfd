7e04ae393e0806f259ee9d381b2f36916be6b706
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-08-26 11:03:17 -0400

    refactor remove_voicemail, add move_voicemail

--- xivo_sysconf/modules/asterisk.py
+++ xivo_sysconf/modules/asterisk.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011-2013,2015 Avencall
+# Copyright (C) 2011-2015 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,40 +23,74 @@ from xivo import http_json_server
 from xivo.http_json_server import HttpReqError
 from xivo.http_json_server import CMD_R
 
+ASTERISK_USER = 'asterisk'
+ASTERISK_GROUP = 'asterisk'
+
 
 class Asterisk(object):
 
     def __init__(self, base_vmail_path='/var/spool/asterisk/voicemail'):
         self._base_vmail_path = base_vmail_path
         self.remove_directory = _remove_directory
+        self.move_directory = _move_directory
         self.is_valid_path_component = _is_valid_path_component
 
     def delete_voicemail(self, args, options):
-        """Delete spool dir associated with voicemail
+        if 'mailbox' not in options:
+            raise HttpReqError(400, "missing 'mailbox' arg", json=True)
 
-            options:
-                name    : voicemail name
-                context : voicemail context (opt. default is 'default')
-        """
-        if 'name' not in options:
-            raise HttpReqError(400, "missing 'name' arg", json=True)
         context = options.get('context', 'default')
-        name = options['name']
+        mailbox = options['mailbox']
 
         if not self.is_valid_path_component(context):
             raise HttpReqError(400, 'invalid context')
-        if not self.is_valid_path_component(name):
-            raise HttpReqError(400, 'invalid name')
+        if not self.is_valid_path_component(mailbox):
+            raise HttpReqError(400, 'invalid mailbox')
 
-        vmpath = os.path.join(self._base_vmail_path, context, name)
+        vmpath = os.path.join(self._base_vmail_path, context, mailbox)
         self.remove_directory(vmpath)
 
         return True
+
+    def move_voicemail(self, args, options):
+        self._validate_options(options)
+
+        old_path = os.path.join(self._base_vmail_path,
+                                options['old_context'],
+                                options['old_mailbox'])
+        new_path = os.path.join(self._base_vmail_path,
+                                options['new_context'],
+                                options['new_mailbox'])
+
+        self.move_directory(old_path, new_path)
+
+        return True
+
+    def _validate_options(self, options):
+        for param in ('old_context', 'old_mailbox', 'new_context', 'new_mailbox'):
+            value = options.get(param)
+            if not value:
+                raise HttpReqError(400, "missing '{}' arg".format(param),
+                                   json=True)
+            if not self.is_valid_path_component(value):
+                raise HttpReqError(400, 'invalid {}'.format(param))
 
 
 def _remove_directory(path):
     if os.path.exists(path):
         shutil.rmtree(path)
+
+
+def _move_directory(old_path, new_path):
+    dirname = os.path.dirname(new_path)
+    commands = [
+        ["rm", "-rf", new_path],
+        ["install", "-d", "-m", "750", "-o", ASTERISK_USER, "-g", ASTERISK_GROUP, dirname],
+        ["mv", old_path, new_path]
+    ]
+
+    for cmd in commands:
+        subprocess.check_call(cmd)
 
 
 def _is_valid_path_component(path_component):
@@ -100,6 +134,8 @@ def _exec_asterisk_command(command):
 asterisk = Asterisk()
 http_json_server.register(asterisk.delete_voicemail, CMD_R,
                           name='delete_voicemail')
+http_json_server.register(asterisk.move_voicemail, CMD_R,
+                          name='move_voicemail')
 http_json_server.register(core_show_version, CMD_R,
                           name='core_show_version')
 http_json_server.register(core_show_channels, CMD_R,
