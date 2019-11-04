7bf233b617a637262d8b0773cf988d32a13d233e
Pascal Cadotte Michaud <pcm@wazo.io>
2019-11-04 15:24:59 -0500

    resolv_conf: add unit tests for the resolv.conf schema validation

--- xivo_sysconf/modules/tests/test_resolvconf.py
+++ xivo_sysconf/modules/tests/test_resolvconf.py
@@ -14,6 +14,7 @@ from hamcrest import (
 from ..resolvconf import (
     HttpReqError,
     _validate_hosts as validate_hosts,
+    _validate_resolv_conf as validate_resolv_conf,
 )
 
 
@@ -67,3 +68,83 @@ class TestValidateHosts(unittest.TestCase):
             calling(validate_hosts).with_args(body),
             not_(raises(HttpReqError)),
         )
+
+
+class TestValidateResolvConf(unittest.TestCase):
+
+    def test_nameservers(self):
+        assert_that(
+            calling(validate_resolv_conf).with_args({}),
+            raises(HttpReqError),
+        )
+
+        assert_that(
+            calling(validate_resolv_conf).with_args({'nameservers': []}),
+            raises(HttpReqError),
+        )
+
+        assert_that(
+            calling(validate_resolv_conf).with_args(
+                {'nameservers': ['10.0.0.1', '10.0.0.2', '10.0.0.3', '10.0.0.4']},
+            ),
+            raises(HttpReqError),
+        )
+
+        invalid_ip_or_domain = [
+            '-foo.wazo-platform.org',
+            None,
+            'a' * 64,
+            '127.0.0.-1',
+        ]
+
+        for ip_or_domain in invalid_ip_or_domain:
+            assert_that(
+                calling(validate_resolv_conf).with_args(
+                    {'nameservers': [ip_or_domain]},
+                ),
+                raises(HttpReqError),
+                ip_or_domain,
+            )
+
+    def test_search(self):
+        assert_that(
+            calling(validate_resolv_conf).with_args({'nameservers': ['valid'], 'search': []}),
+            raises(HttpReqError),
+        )
+
+        assert_that(
+            calling(validate_resolv_conf).with_args(
+                {
+                    'nameservers': ['valid'],
+                    'search': [
+                        'toto.1.tld',
+                        'toto.2.tld',
+                        'toto.3.tld',
+                        'toto.4.tld',
+                        'toto.5.tld',
+                        'toto.6.tld',
+                        'toto.7.tld',
+                    ],
+                },
+            ),
+            raises(HttpReqError),
+        )
+
+        invalid_names = [
+            64 * 'a' + '.wazo-platform.org',
+            '-NOT',
+            'NOT-',
+            'foo!bar',
+            'foo.-NOT.bar',
+        ]
+
+        for name in invalid_names:
+            body = {
+                'nameservers': ['valid'],
+                'search': [name],
+            }
+            assert_that(
+                calling(validate_resolv_conf).with_args(body),
+                raises(HttpReqError),
+                name,
+            )
