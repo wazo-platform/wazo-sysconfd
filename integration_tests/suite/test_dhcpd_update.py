c755d542d377501bdbc1b63ac4b92b2e574e499a
Sebastien Duthil <sduthil@wazo.community>
2021-02-19 18:51:55 -0500

    test delete_voicemail

--- integration_tests/suite/test_dhcpd_update.py
+++ integration_tests/suite/test_dhcpd_update.py
@@ -25,3 +25,24 @@ class TestSysconfd(IntegrationTest):
             )
 
         until.true(command_was_called, ['dhcpd-update', '-dr'], timeout=5)
+
+    def test_delete_voicemail(self):
+        voicemail_directory = '/var/spool/asterisk/voicemail/mycontext/myvoicemail'
+        self._create_directory(voicemail_directory)
+
+        self.sysconfd.delete_voicemail(mailbox='myvoicemail', context='mycontext')
+
+        assert(not self._directory_exists(voicemail_directory))
+
+    def _create_directory(self, directory):
+        self.docker_exec(['mkdir', '-p', directory], 'sysconfd')
+
+    def _directory_exists(self, directory):
+        command = ' '.join(['test', '-d', directory, '&&', 'echo', 'true', '||', 'echo', 'false'])
+        out = self.docker_exec(['sh', '-c', command], 'sysconfd').strip().decode('utf-8')
+        if out == 'true':
+            return True
+        elif out == 'false':
+            return False
+        else:
+            raise RuntimeError(f'Unknown output: "{out}"')
