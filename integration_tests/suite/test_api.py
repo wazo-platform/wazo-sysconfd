0f13d0fe289003bd148b8a0ec86ebb09a4944424
Sebastien Duthil <sduthil@wazo.community>
2021-02-23 16:39:38 -0500

    test exec_request_handlers

--- integration_tests/suite/test_api.py
+++ integration_tests/suite/test_api.py
@@ -61,6 +61,39 @@ class TestSysconfd(IntegrationTest):
         until.true(command_was_called, ['xivo-update-config'], timeout=5)
         until.true(command_was_called, ['xivo-monitoring-update'], timeout=5)
 
+    def test_exec_request_handlers(self):
+        agent_id = 12
+        asterisk_command = 'dialplan reload'
+        autoprov_filename = '/etc/asterisk/pjsip.d/05-autoprov-wizard.conf'
+        self._create_file(autoprov_filename, owner='root')
+        agent_bus_events = self.bus.accumulator('config.agent.edited')
+        command_bus_events = self.bus.accumulator('sysconfd.sentinel')
+        body = {
+            'ipbx': [asterisk_command],
+            'agentbus': [f'agent.edit.{agent_id}'],
+            'chown_autoprov_config': ['something'],
+        }
+
+        self.sysconfd.exec_request_handlers(body)
+
+        def command_was_called(command):
+            return any(
+                message for message in command_bus_events.accumulate()
+                if message['name'] == 'sysconfd_sentinel'
+                and message['data']['command'] == command
+            )
+
+        def agent_event_was_sent():
+            return any(
+                message for message in agent_bus_events.accumulate()
+                if message['name'] == 'agent_edited'
+                and message['data']['id'] == agent_id
+            )
+
+        until.true(command_was_called, ['asterisk', '-rx', asterisk_command], timeout=5)
+        until.true(agent_event_was_sent, timeout=5)
+        assert(self._file_owner(autoprov_filename) == 'asterisk')
+
     def _create_directory(self, directory):
         self.docker_exec(['mkdir', '-p', directory], 'sysconfd')
 
@@ -73,3 +106,9 @@ class TestSysconfd(IntegrationTest):
             return False
         else:
             raise RuntimeError(f'Unknown output: "{out}"')
+
+    def _create_file(self, file_name, owner):
+        self.docker_exec(['install', '-D', '-o', owner, '/dev/null', file_name], 'sysconfd')
+
+    def _file_owner(self, file_name):
+        return self.docker_exec(['stat', '-c', '%U', file_name], 'sysconfd').strip().decode('utf-8')
