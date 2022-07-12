c8e0eeaf1a5a2900e1658fc499dce31653f2aa73
Alexandre Fournier <afournier@wazo.io>
2022-07-12 17:32:35 -0400

    settings: add copyright

--- wazo_sysconfd/settings.py
+++ wazo_sysconfd/settings.py
@@ -1,3 +1,6 @@
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from functools import lru_cache
 
 from wazo_sysconfd.plugins.asterisk.asterisk import Asterisk
