e24d4312f1fbe3c3c35124cf0e8289b1def6d174
nissou91 <achohra@wazo.io>
2022-07-19 10:55:53 -0400

    Move safe_init call to API init

--- wazo_sysconfd/plugins/hosts/http.py
+++ wazo_sysconfd/plugins/hosts/http.py
@@ -1,20 +1,11 @@
 # Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import os
 from fastapi import APIRouter, Body
-from wazo_sysconfd.modules.resolvconf import safe_init, Hosts
-from xivo.config_helper import parse_config_file
-
-
-SYSCONFD_CONFIGURATION_FILE = os.path.join('/etc/wazo-sysconfd', 'config.yml')
+from wazo_sysconfd.modules.resolvconf import Hosts
 
 router = APIRouter()
 
 @router.post('/hosts', status_code=200)
 def update_files(body: dict = Body()):
-    
-    options = parse_config_file(SYSCONFD_CONFIGURATION_FILE)
-
-    safe_init(options)
     Hosts(body)
