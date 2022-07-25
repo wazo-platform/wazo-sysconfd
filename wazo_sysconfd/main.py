17f5faedcfd961f42b605856d3a5cdb4ee4329b3
Nabil Ben Youssef <nabil.benyoussef@wazo.io>
2022-07-25 17:55:49 +0200

    Migrate /exec_request_handlers using FastApi + Python3
    As Python 2 is no more maintained, this endpoint is migrated to Python 3
    with FastAPI.

--- wazo_sysconfd/main.py
+++ wazo_sysconfd/main.py
@@ -8,9 +8,14 @@ from xivo.xivo_logging import get_log_level_by_name, setup_logging
 from .controller import Controller
 from .config import load_config
 
+config = None
+
 
 def main():
+    global config
     config = load_config(sys.argv[1:])
-    setup_logging(config['log_file'], log_level=get_log_level_by_name(config['log_level']))
+    setup_logging(
+        config['log_file'], log_level=get_log_level_by_name(config['log_level'])
+    )
     controller = Controller(config)
     controller.run()
--- wazo_sysconfd/settings.py
+++ wazo_sysconfd/settings.py
@@ -3,9 +3,19 @@
 
 from functools import lru_cache
 
+from wazo_sysconfd.main import config
 from wazo_sysconfd.plugins.asterisk.asterisk import Asterisk
+from wazo_sysconfd.request_handlers.request import RequestHandlersProxy
 
 
 @lru_cache()
 def get_asterisk():
     return Asterisk()
+
+
+@lru_cache()
+def get_request_handlers_proxy():
+    request_handlers_proxy = RequestHandlersProxy()
+    request_handlers_proxy.safe_init_from_config(config)
+    request_handlers_proxy.at_start(None)
+    return request_handlers_proxy
