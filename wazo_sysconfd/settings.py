827236ae38f99dfb41d746f94fbcfe6956beb9fe
Sebastien Duthil <sduthil@wazo.community>
2022-07-26 14:14:30 -0400

    ha: move dependencies to plugin

--- wazo_sysconfd/settings.py
+++ wazo_sysconfd/settings.py
@@ -4,18 +4,8 @@
 from functools import lru_cache
 
 from wazo_sysconfd.plugins.asterisk.asterisk import Asterisk
-from wazo_sysconfd.plugins.ha_config.ha import (
-    HAConfigManager,
-    _PostgresConfigUpdater,
-    _CronFileInstaller,
-)
 
 
 @lru_cache()
 def get_asterisk():
     return Asterisk()
-
-
-@lru_cache()
-def get_ha_config_manager():
-    return HAConfigManager(_PostgresConfigUpdater, _CronFileInstaller())
