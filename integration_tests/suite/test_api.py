cd53f84868d239d2269f3362a36fa4c97fec368c
Sebastien Duthil <sduthil@wazo.community>
2022-07-26 16:43:11 -0400

    hosts: fix config parsing

--- integration_tests/suite/test_api.py
+++ integration_tests/suite/test_api.py
@@ -114,8 +114,8 @@ class TestSysconfd(IntegrationTest):
         )
 
     def test_hosts(self):
-        self._given_file_absent('/etc/hostname')
-        self._given_file_absent('/etc/hosts')
+        self._given_file_absent('/etc/local/hostname')
+        self._given_file_absent('/etc/local/hosts')
         bus_events = self.bus.accumulator('sysconfd.sentinel')
         body = {
             'hostname': 'wazo',
@@ -124,10 +124,10 @@ class TestSysconfd(IntegrationTest):
 
         self.sysconfd.hosts(body)
 
-        expected_command = ['hostname', '-F', '/etc/hostname']
+        expected_command = ['hostname', '-F', '/etc/local/hostname']
         assert self._command_was_called(bus_events, expected_command)
-        assert self._file_exists('/etc/hostname')
-        assert self._file_exists('/etc/hosts')
+        assert self._file_exists('/etc/local/hostname')
+        assert self._file_exists('/etc/local/hosts')
 
     @pytest.mark.skip(reason=FASTAPI_REASON)
     def test_resolv_conf(self):
