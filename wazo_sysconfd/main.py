b303b2af5bb0eb88a9ee97c412374628c561dc74
Nabil Ben Youssef <nabil.benyoussef@wazo.io>
2022-07-26 18:29:14 +0200

    config is no more defined as global
    As the configuration is available with the dependencies, the 'config' variable
    doesn't need to be set as global.

--- wazo_sysconfd/main.py
+++ wazo_sysconfd/main.py
@@ -8,11 +8,8 @@ from xivo.xivo_logging import get_log_level_by_name, setup_logging
 from .controller import Controller
 from .config import load_config
 
-config = None
-
 
 def main():
-    global config
     config = load_config(sys.argv[1:])
     setup_logging(
         config['log_file'], log_level=get_log_level_by_name(config['log_level'])
