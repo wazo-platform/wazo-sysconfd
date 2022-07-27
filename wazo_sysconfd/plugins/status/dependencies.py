6c55fc232803166ce5f43d8a153fb087c44cb011
Sebastien Duthil <sduthil@wazo.community>
2022-07-27 09:40:55 -0400

    status: extract dependencies to its own module

---
+++ wazo_sysconfd/plugins/status/dependencies.py
@@ -0,0 +1,11 @@
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functools import lru_cache
+
+status_aggregator = None
+
+
+@lru_cache()
+def get_status_aggregator():
+    return status_aggregator
