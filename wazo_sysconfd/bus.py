6072da100d076c74e21e31a0e703508efa51f7c6
Julien Alie <jalie@wazo.io>
2023-08-24 13:58:51 -0400

    add BusConsumer to sysconfd

---
+++ wazo_sysconfd/bus.py
@@ -0,0 +1,116 @@
+# Copyright 2023 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import annotations
+from typing import Callable
+
+from multiprocessing.managers import BaseManager, BaseProxy
+
+from xivo.status import Status, StatusDict
+from xivo_bus.consumer import BusConsumer as Consumer
+from xivo_bus.publisher import BusPublisher as Publisher
+
+from .plugins.request_handlers import dependencies as request_handlers_deps
+
+
+class BusConsumer(Consumer):
+    def __init__(self, wazo_uuid, **bus_config):
+        super().__init__('wazo-sysconfd', **bus_config)
+        self._wazo_uuid: str = wazo_uuid
+
+    @classmethod
+    def from_config(cls, config: dict):
+        return cls(config['uuid'], **config['bus'])
+
+    # Override ConsumerMixin to add headers to callbacks
+    def _ConsumerMixin__dispatch(
+        self, event_name: str, payload: dict, headers: dict = None
+    ):
+        with self._ConsumerMixin__lock:
+            subscriptions = self._ConsumerMixin__subscriptions[event_name].copy()
+        for handler, _ in subscriptions:
+            try:
+                handler(payload, headers)
+            except Exception:
+                self.log.exception(
+                    'Handler \'%s\' for event \'%s\' failed',
+                    getattr(handler, '__name__', handler),
+                    event_name,
+                )
+
+
+class BusConsumerProxy(BaseProxy):
+    _exposed_ = (
+        '__getattribute__',
+        'consumer_connected',
+        'provide_status',
+        'subscribe',
+        'unsubscribe',
+    )
+
+    def consumer_connected(self) -> bool:
+        return self._callmethod('consumer_connected', ())
+
+    def provide_status(self, status: StatusDict) -> None:
+        status['bus_consumer']['status'] = (
+            Status.ok if self._callmethod('consumer_connected', ()) else Status.fail
+        )
+
+    def subscribe(
+        self,
+        event_name: str,
+        handler: Callable[[dict, dict], None],
+        headers: dict | None = None,
+        routing_key: str | None = None,
+        headers_match_all: bool = True,
+    ) -> None:
+        return self._callmethod(
+            'subscribe', (event_name, handler, headers, routing_key, headers_match_all)
+        )
+
+    def unsubscribe(
+        self, event_name: str, handler: Callable[[dict, dict], None]
+    ) -> None:
+        return self._callmethod('unsubscribe', (event_name, handler))
+
+    @property
+    def is_running(self) -> bool:
+        return self._callmethod('__getattribute__', ('is_running',))
+
+
+class BusManager:
+    class _ProxyManager(BaseManager):
+        pass
+
+    def __init__(self, config: dict):
+        self._bus_consumer: BusConsumer | None = None
+        self._config: dict = config
+        self._manager = self._ProxyManager()
+        self._manager.register(
+            'bus_consumer', lambda: self._bus_consumer, BusConsumerProxy
+        )
+
+    def _initialize(self):
+        # Note: must call so request_handler callback can work in bus process, since
+        # it will be configured after the bus process has already started
+        request_handlers_deps.config = self._config
+
+        self._bus_consumer = BusConsumer.from_config(self._config)
+        self._bus_consumer.start()
+
+    def get_consumer(self) -> BusConsumerProxy:
+        return self._manager.bus_consumer()
+
+    def start(self):
+        self._manager.start(self._initialize)
+
+    def stop(self):
+        self._bus_consumer.stop()
+        self._manager.shutdown()
+        self._manager.join()
+
+
+class BusPublisher(Publisher):
+    @classmethod
+    def from_config(cls, service_uuid: str, bus_config: dict):
+        return cls(name='wazo-sysconfd', service_uuid=service_uuid, **bus_config)
--- wazo_sysconfd/controller.py
+++ wazo_sysconfd/controller.py
@@ -7,7 +7,9 @@ import logging
 from xivo import plugin_helpers
 from xivo.status import StatusAggregator
 
+from .bus import BusManager
 from .http_server import api, SysconfdApplication
+
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +17,11 @@ logger = logging.getLogger(__name__)
 class Controller:
     def __init__(self, config: dict):
         self.config = config
+        self.bus_manager = BusManager(config)
         self.http_server = SysconfdApplication('%(prog)s', config=config)
         self.status_aggregator = StatusAggregator()
+
+        self.bus_manager.start()
 
         plugin_manager = plugin_helpers.load(
             namespace='wazo_sysconfd.plugins',
@@ -24,6 +29,7 @@ class Controller:
             dependencies={
                 'api': api,
                 'config': config,
+                'get_bus_consumer': self.bus_manager.get_consumer,
                 'status_aggregator': self.status_aggregator,
             },
         )
@@ -36,4 +42,7 @@ class Controller:
 
     def run(self):
         logger.info('wazo-sysconfd starting...')
-        self.http_server.run()
+        try:
+            self.http_server.run()
+        finally:
+            self.bus_manager.stop()
